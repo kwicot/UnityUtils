27024cbb898f89556af726124683376e583537cb
Andrii_PC <kwicot@gmail.com>
2024-07-23 00:07:01 +0300

    StringExtension

---
+++ Scripts/Utils/Extensions/StringExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace Kwicot.Core.Scripts.Utils.Extensions
+{
+    public static class StringExtension
+    {
+        public static bool IsNull(this string value) => string.IsNullOrEmpty(value);
+        public static bool IsNotNull(this string value) => !string.IsNullOrEmpty(value);
+        public static bool IsEmpty(this string value) => string.IsNullOrEmpty(value);
+        public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
+        public static bool IsNullOrWhiteSpace(this string value) => string.IsNullOrWhiteSpace(value);
+        public static bool IsNotEmpty(this string value) => !IsEmpty(value);
+        public static bool IsNotNullOrWhiteSpace(this string value) => !IsNullOrWhiteSpace(value);
+        
+    }
+}
